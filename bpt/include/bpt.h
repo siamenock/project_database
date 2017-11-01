03f0492845cf52d67b18a7ffba5005d52ce36ba0
siamenock <siamenock@gmail.com>
2017-11-01 21:45:07 +0900

    Update bpt.h

--- bpt/include/bpt.h
+++ bpt/include/bpt.h
@@ -1,10 +1,14 @@
 #ifndef __BPT_H__
 #define __BPT_H__
 
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_DEPRECATE
+
 // Uncomment the line below if you are compiling on Windows.
 // #define WINDOWS
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <stdbool.h>
 #ifdef WINDOWS
 #define bool char
@@ -63,95 +67,99 @@
 // TYPES.
 
 /* Type representing the record
- * to which a given key refers.
- * In a real B+ tree system, the
- * record would hold data (in a database)
- * or a file (in an operating system)
- * or some other information.
- * Users can rewrite this part of the code
- * to change the type and content
- * of the value field.
- */
+* to which a given key refers.
+* In a real B+ tree system, the
+* record would hold data (in a database)
+* or a file (in an operating system)
+* or some other information.
+* Users can rewrite this part of the code
+* to change the type and content
+* of the value field.
+*/
 typedef unsigned long Offset;
-typedef struct LeafRecord{      //leaf node's record
-        int64_t key;            //not support internal node
-        char value[VALUE_SIZE];
-}LeafRecord;            
+typedef struct IntrRecord {
+	int64_t key;            //not support internal node
+	Offset offset;
+} IntrRecord;
+typedef struct LeafRecord {      //leaf node's record
+	int64_t key;            //not support internal node
+	char value[VALUE_SIZE];
+}LeafRecord;
 
 typedef struct record {
-    int value;
+	int value;
 } record;
 
 /* Type representing a node in the B+ tree.
- * This type is general enough to serve for both
- * the leaf and the internal node.
- * The heart of the node is the array
- * of keys and the array of corresponding
- * pointers.  The relation between keys
- * and pointers differs between leaves and
- * internal nodes.  In a leaf, the index
- * of each key equals the index of its corresponding
- * pointer, with a maximum of order - 1 key-pointer
- * pairs.  The last pointer points to the
- * leaf to the right (or NULL in the case
- * of the rightmost leaf).
- * In an internal node, the first pointer
- * refers to lower nodes with keys less than
- * the smallest key in the keys array.  Then,
- * with indices i starting at 0, the pointer
- * at i + 1 points to the subtree with keys
- * greater than or equal to the key in this
- * node at index i.
- * The num_keys field is used to keep
- * track of the number of valid keys.
- * In an internal node, the number of valid
- * pointers is always num_keys + 1.
- * In a leaf, the number of valid pointers
- * to data is always num_keys.  The
- * last leaf pointer points to the next leaf.
- */
+* This type is general enough to serve for both
+* the leaf and the internal node.
+* The heart of the node is the array
+* of keys and the array of corresponding
+* pointers.  The relation between keys
+* and pointers differs between leaves and
+* internal nodes.  In a leaf, the index
+* of each key equals the index of its corresponding
+* pointer, with a maximum of order - 1 key-pointer
+* pairs.  The last pointer points to the
+* leaf to the right (or NULL in the case
+* of the rightmost leaf).
+* In an internal node, the first pointer
+* refers to lower nodes with keys less than
+* the smallest key in the keys array.  Then,
+* with indices i starting at 0, the pointer
+* at i + 1 points to the subtree with keys
+* greater than or equal to the key in this
+* node at index i.
+* The num_keys field is used to keep
+* track of the number of valid keys.
+* In an internal node, the number of valid
+* pointers is always num_keys + 1.
+* In a leaf, the number of valid pointers
+* to data is always num_keys.  The
+* last leaf pointer points to the next leaf.
+*/
 typedef struct node {
-    void ** pointers;
-    int * keys;
-    struct node * parent;
-    bool is_leaf;
-    int num_keys;
-    struct node * next; // Used for queue.
+	void ** pointers;
+	int * keys;
+	struct node * parent;
+	bool is_leaf;
+	int num_keys;
+	struct node * next; // Used for queue.
 } node;
 
 // GLOBALS.
 
 /* The order determines the maximum and minimum
- * number of entries (keys and pointers) in any
- * node.  Every node has at most order - 1 keys and
- * at least (roughly speaking) half that number.
- * Every leaf has as many pointers to data as keys,
- * and every internal node has one more pointer
- * to a subtree than the number of keys.
- * This global variable is initialized to the
- * default value.
- */
+* number of entries (keys and pointers) in any
+* node.  Every node has at most order - 1 keys and
+* at least (roughly speaking) half that number.
+* Every leaf has as many pointers to data as keys,
+* and every internal node has one more pointer
+* to a subtree than the number of keys.
+* This global variable is initialized to the
+* default value.
+*/
 extern int order;
 
 /* The queue is used to print the tree in
- * level order, starting from the root
- * printing each entire rank on a separate
- * line, finishing with the leaves.
- */
+* level order, starting from the root
+* printing each entire rank on a separate
+* line, finishing with the leaves.
+*/
 extern node * queue;
 
 /* The user can toggle on and off the "verbose"
- * property, which causes the pointer addresses
- * to be printed out in hexadecimal notation
- * next to their corresponding keys.
- */
+* property, which causes the pointer addresses
+* to be printed out in hexadecimal notation
+* next to their corresponding keys.
+*/
 extern bool verbose_output;
 
 
 
 /* Only one file opened as main DB
- * to prevent using this file pointer on every read/write, It became global
- */
+* to prevent using this file pointer on every read/write, It became global
+*/
 extern FILE* dbfile;
 
 
@@ -168,10 +176,10 @@ int height( node * root );
 int path_to_root( node * root, node * child );
 void print_leaves( node * root );
 void print_tree( node * root );
-/void find_and_print(node * root, int key, bool verbose); 
-void find_and_print_range(node * root, int range1, int range2, bool verbose); 
+/void find_and_print(node * root, int key, bool verbose);
+void find_and_print_range(node * root, int range1, int range2, bool verbose);
 int find_range( node * root, int key_start, int key_end, bool verbose,
-        int returned_keys[], void * returned_pointers[]); 
+int returned_keys[], void * returned_pointers[]);
 node * find_leaf( node * root, int key, bool verbose );
 record * find( node * root, int key, bool verbose );
 int cut( int length );
@@ -179,8 +187,8 @@ int cut( int length );
 // Insertion.
 
 
-Offset make_node( void );
-Offset make_leaf( void );
+Offset make_node(void);
+Offset make_leaf(void);
 int get_left_index(node * parent, node * left);
 
 int insert_into_leaf_after_splitting(Offset leaf, int key, LeafRecord r);
@@ -188,29 +196,32 @@ int insert_into_leaf_after_splitting(Offset leaf, int key, LeafRecord r);
 //under here, my funtions
 void FileInit(FILE* fp);
 
+void MoreFreePage();
 void SetInstancesOnDB(Offset node_offset, void* value, int instance_pos, size_t size, size_t count);
 Offset GetOffsetOnDB(Offset node_offset, int instance_pos);
 int32_t GetInt32OnDB(Offset node_offset, int instance_pos);
 
 void SetNextFreePage(Offset node_offset, Offset value);
-void SetParentPage  (Offset node_offset, Offset value);
+void SetParentPage(Offset node_offset, Offset value);
 void SetRightSibling(Offset node_offset, Offset value);
-void SetIsLeaf      (Offset node_offset,int32_t value);
-void SetKeyNum      (Offset node_offset,int32_t value);
-void SetHeadersPageNum(                 int32_t value);
-void SetHeadersRootPage(                 Offset value);
-
+void SetIsLeaf(Offset node_offset, int32_t value);
+void SetKeyNum(Offset node_offset, int32_t value);
+void SetHeadersPageNum(int32_t value);
+void SetHeadersRootPage(Offset value);
+void SetLeafRecord(Offset node_offset, int index, LeafRecord r);
+void SetIntrRecord(Offset node_offset, int index, IntrRecord r);
 
 Offset GetNextFreePage(Offset node_offset);
-Offset GetParentPage  (Offset node_offset);
+Offset GetParentPage(Offset node_offset);
 Offset GetRightSibling(Offset node_offset);
-int32_t GetIsLeaf     (Offset node_offset);
-int32_t GetKeyNum     (Offset node_offset);
-int32_t GetHeadersPageNum(               );
-Offset GetHeadersRootPage(               );
-Offset  GetChild (Offset node_offset, int index);
-int64_t GetKey   (Offset node_offset, int index);
+int32_t GetIsLeaf(Offset node_offset);
+int32_t GetKeyNum(Offset node_offset);
+int32_t GetHeadersPageNum();
+Offset GetHeadersRootPage();
+Offset  GetChild(Offset node_offset, int index);
+int64_t GetKey(Offset node_offset, int index);
 char* GetValuePtr(Offset node_offset, int index);
+
 
 
 #endif /* __BPT_H__*/
